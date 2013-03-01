31a260fa7254dae62a635645bd5df31f872f98da
Chase Seibert <chase.seibert+github@gmail.com>
2013-03-01 10:00:48 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author='Chase Seibert',
     author_email='chase.seibert@gmail.com',
     packages=['migratron'],
-    url='https://github.com/chase-seibert/migratron',
+    url='https://github.com/chase-seibert/django-migratron',
+    download_url='https://github.com/chase-seibert/django-migratron/tarball/master',
     license='LICENSE.txt',
     description='Create and run different buckets of unordered schema and data migrations.',
-    long_description=open('README.md').read(),
 )
